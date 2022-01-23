dd74e5ea00a60f23f833f2e6e099c4d26667fad5
salihcanbinboga <salihcanbinboga@gmail.com>
2022-01-23 14:33:41 +0300

    create movies detail and movies fragment, complete ui design. Create list item design

--- app/src/main/java/com/example/scipandroidworkshop2/ui/MainActivity.kt
+++ app/src/main/java/com/example/scipandroidworkshop2/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.scipandroidworkshop2
+package com.example.scipandroidworkshop2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.scipandroidworkshop2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/scipandroidworkshop2/ui/screens/movies/MoviesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.scipandroidworkshop2.ui.screens.movies
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.scipandroidworkshop2.R
+
+
+class MoviesFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+}
