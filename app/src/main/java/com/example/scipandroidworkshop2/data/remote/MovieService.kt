6ab8916a3f82bee2951f30129210e4adc6eaab9a
salihcanbinboga <salihcanbinboga@gmail.com>
2022-01-23 14:40:44 +0300

    add movies service instance creator logic

--- app/src/main/java/com/example/scipandroidworkshop2/data/remote/MovieService.kt
+++ app/src/main/java/com/example/scipandroidworkshop2/data/remote/MovieService.kt
@@ -2,6 +2,9 @@ package com.example.scipandroidworkshop2.data.remote
 
 import com.example.scipandroidworkshop2.data.models.MoviesResponseModel
 import com.example.scipandroidworkshop2.data.models.Results
+import com.example.scipandroidworkshop2.utils.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -17,4 +20,18 @@ interface MovieService {
         @Query("api_key") apiKey: String
     ): Results
 
+    companion object {
+        var retrofitService: MovieService? = null
+        fun getInstance(): MovieService {
+            if (retrofitService == null) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                retrofitService = retrofit.create(MovieService::class.java)
+            }
+            return retrofitService!!
+        }
+
+    }
 }
--- app/src/main/java/com/example/scipandroidworkshop2/data/repositories/MoviesRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.scipandroidworkshop2.data.repositories
-
-import com.example.scipandroidworkshop2.data.models.MoviesResponseModel
-import com.example.scipandroidworkshop2.data.models.Results
-
-interface MoviesRepository {
-    suspend fun getUpComingMovies(): MoviesResponseModel
-    suspend fun getMovie(movieID: String): Results
-}
