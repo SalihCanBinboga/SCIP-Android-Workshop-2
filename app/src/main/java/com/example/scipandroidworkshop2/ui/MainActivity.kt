5abde272dc450284d3bfe6d7e8c265bc3a0bad3b
salihcanbinboga <salihcanbinboga@gmail.com>
2022-01-23 16:55:36 +0300

    navigation component toolbar back button listen settings

--- app/src/main/java/com/example/scipandroidworkshop2/ui/MainActivity.kt
+++ app/src/main/java/com/example/scipandroidworkshop2/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.scipandroidworkshop2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.scipandroidworkshop2.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationHost =
+            supportFragmentManager.findFragmentById(
+                R.id.fragmentContainerView
+            ) as NavHostFragment
+        navController = navigationHost.navController
+
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            navController
+        )
+
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
     }
 }
